491c773f7a60b9ab819ec725c46b880d2e5156b9
Oster.Huang <oster.huang@gramltd.com>
2023-01-10 18:29:07 +0800

    Image Pager with UIKit integration

--- try.switfui/view/category/CategoryHome.swift
+++ try.switfui/view/category/CategoryHome.swift
@@ -14,19 +14,19 @@ struct CategoryHome: View {
     var body: some View {
         
         NavigationView {
-            List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
-                    .listRowInsets(EdgeInsets())
-                
-                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    CategoryRow(categoryName: key, categoryLandmarks: modelData.categories[key]!)
-                        .padding(.vertical)
-                }
+            ScrollView {
+                VStack {
+                    PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                        .aspectRatio(3 / 2, contentMode: .fit)
+                        .listRowInsets(EdgeInsets())
                     
+                    ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                        CategoryRow(categoryName: key, categoryLandmarks: modelData.categories[key]!)
+                            .padding(.vertical)
+                            .padding(.horizontal)
+                    }
+                    
+                }
             }.listRowInsets(EdgeInsets())
             .navigationTitle("Featured")
             .toolbar {
--- try.switfui/view/component/pageView/PageView.swift
+++ try.switfui/view/component/pageView/PageView.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct PageView: View {
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView()
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+                    .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
